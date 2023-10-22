c21795f37af34a2222450ed07bb49d58c844d504
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-23 00:39:46 +0500

    GameScreen Can Now Be Closed!

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ game_window_size = (game_window_width, game_window_height)
 screen = pygame.display.set_mode(game_window_size)
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
+
     pygame.display.update()
