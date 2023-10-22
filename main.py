3aedb7e9fa2e917787d3fb844452c3f7b87f9f3c
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-23 00:37:11 +0500

    GameScreen Updating In While True

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ game_window_width, game_window_height = 800, 400
 game_window_size = (game_window_width, game_window_height)
 
 screen = pygame.display.set_mode(game_window_size)
+
+while True:
+    pygame.display.update()
