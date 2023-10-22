fab21c3f28791d418d9a953001b253837da72cb0
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-23 00:43:02 +0500

    Adding Game Title!

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from sys import exit
 
 pygame.init()
 
+game_title = "Runner"
+
 game_window_width, game_window_height = 800, 400
 game_window_size = (game_window_width, game_window_height)
 
 screen = pygame.display.set_mode(game_window_size)
+pygame.display.set_caption(game_title)
 
 while True:
     for event in pygame.event.get():
