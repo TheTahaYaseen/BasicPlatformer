7775aac6490ee6530fc20a7614935e22ae191a0c
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-23 00:55:28 +0500

    Introduced Plain Surface!

--- main.py
+++ main.py
@@ -14,11 +14,19 @@ pygame.display.set_caption(game_title)
 
 clock = pygame.time.Clock()
 
+# plain_color_surface = pygame.Surface(width, height)
+plain_color_surface = pygame.Surface(100, 200)
+# plain_color_surface.fill(color)
+plain_color_surface.fill("Blue")
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    # screen.blit(surface_to_put_on_display, position)
+    screen.blit(plain_color_surface, (0, 0))
 
     pygame.display.update()
     clock.tick(game_framerate)
