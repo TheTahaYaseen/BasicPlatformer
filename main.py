0a73a1cf2c87c8dfa795452de34cf67ac2d5d784
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-23 00:35:42 +0500

    Introduced GameScreen

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import pygame
 
 pygame.init()
+
+game_window_width, game_window_height = 800, 400
+game_window_size = (game_window_width, game_window_height)
+
+screen = pygame.display.set_mode(game_window_size)
