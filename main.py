090951f881d0d931ff6a9febdac96f3f70fd3bad
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-23 00:46:27 +0500

    Introduced Framerate!

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from sys import exit
 pygame.init()
 
 game_title = "Runner"
+game_framerate = 60
 
 game_window_width, game_window_height = 800, 400
 game_window_size = (game_window_width, game_window_height)
 
 screen = pygame.display.set_mode(game_window_size)
 pygame.display.set_caption(game_title)
+
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -18,3 +21,4 @@ while True:
             exit()
 
     pygame.display.update()
+    clock.tick(game_framerate)
